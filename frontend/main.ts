0198a6dd535a77cc140f9cef684c3f16667171a7
Marco <67731379+MarcoPellero@users.noreply.github.com>
2022-08-11 14:39:24 +0200

    revert whitespace commit

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,7 +1,5 @@
 import { Task, getAllTasks } from "./util/stats.js";
-
 import { getProfile, profileScores } from "./util/training.js";
-
 
 const startYear = 2000;
 const endYear = new Date().getFullYear();
