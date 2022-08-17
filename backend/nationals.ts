12fefb0946b4e66dbd2bea9a499bf9744bbbbddb
marco pellero <marco.pellero.it@gmail.com>
2022-08-17 16:54:31 +0200

    getScore(user) implemented

--- backend/nationals.ts
+++ backend/nationals.ts
@@ -1,5 +1,5 @@
 import fetch from "cross-fetch";
-import * as misc from "./misc";
+import * as misc from "./misc.js";
 
 interface statsMedal {
 	count: number,
@@ -97,6 +97,25 @@ interface statsYear {
 	year: number
 }
 
+interface trainingUser {
+	access_level: number,
+	first_name: string,
+	global_access_level: number,
+	institute: {},
+	join_date: EpochTimeStamp,
+	last_name: string,
+	mail_hash: string,
+	score: number,
+	scores: {
+		name: string,
+		score: number,
+		title: string
+	}[],
+	success: 0 | 1,
+	tasks_solved: number,
+	username: string
+}
+
 export async function getTasks(): Promise<misc.Task[][]> {
 	const id = "gphdM1jkKB5gEYSdkx8UD"; // changes whenever the site is updated; can be scraped from the HTML; i should do that for sure...
 	const url = (year: number) => `https://stats.olinfo.it/_next/data/${id}/contest/${year}.json`;
@@ -132,7 +151,47 @@ export async function getTasks(): Promise<misc.Task[][]> {
 	return output;
 }
 
-(async () => {
-	console.log(misc.range(2000, new Date().getFullYear()+1));
-	console.log(await getTasks());
-})()
+export async function getScores(user: string): Promise<misc.Scores> {
+	const url = "https://training.olinfo.it/api/user";
+	const payload = {
+		action: "get",
+		username: user
+	}
+
+	const res = await fetch(
+		url,
+		{
+			method: "post",
+			body: JSON.stringify(payload),
+			headers: { "Content-Type": "application/json" }
+		});
+	
+	const data: trainingUser = await res.json();
+	console.log(typeof data);
+	if (!data.success)
+		throw new Error(`Can't get scores of user {${user}}: does not exist`);
+	
+	const scores: misc.Scores = {};
+	for (const t of data.scores) {
+		const link = `https://https://training.olinfo.it/#/task/${t.name}/statement`;
+		scores[link] = t.score; // insert by link, because names differ between stats & training
+	}
+
+	return scores;
+}
+
+const debug = async () => {
+	let time;
+
+	time = performance.now();
+	const tasks = await getTasks();
+	console.log(`Time to get tasks: ${time}`);
+	console.log(tasks);
+
+	time = performance.now();
+	const scores = await getScores("v.bizzarri");
+	console.log(`Time to get scores: ${time}`);
+	console.log(scores);
+}
+
+// debug();
