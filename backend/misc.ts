2fb32393ce68031eb960ccfda1f8d9d000a706b8
marco pellero <marco.pellero.it@gmail.com>
2022-08-19 17:54:25 +0200

    better API types

--- backend/misc.ts
+++ backend/misc.ts
@@ -1,12 +1,24 @@
 export interface Task {
 	name: string,
-	title: string,
+	id: any, // any unique data about a certain task
 	link: null | string,
 	score: null | number,
 	max_score_possible: null | number
 }
 
-// {link : score}
-export interface Scores { [key : string] : number }
+export interface Event { // any competition event like IOI2021
+	year: number,
+	tasks: Task[]
+}
+
+export interface userData {
+	user: undefined | string,
+	password: undefined | string
+};
+
+export interface CompHandler {
+	name: string,
+	getTasks(data: userData): Promise<Event[]>
+}
 
 export const range = (from: number, to: number) => Array.from({length: to-from}, (x, i) => from + i);
