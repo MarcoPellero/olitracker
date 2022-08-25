5129697753324d04f6880167c4f030b1d59cb8e7
marco pellero <marco.pellero.it@gmail.com>
2022-08-25 21:26:11 +0200

    added request logging

--- backend/main.ts
+++ backend/main.ts
@@ -10,46 +10,63 @@ const handlers: {[key: string]: misc.CompHandler} = {
 
 const port = process.env.PORT || 8080
 const app = express()
+let unique_req_id = 0; // linearly incrementing ID for every request (for logging)
 
 app.use(express.static("../frontend/"))
 
 app.get("/api/tasks", (req, res) => {
+	const req_id = unique_req_id++;
+
 	const data: misc.ApiQuery = req.query as unknown as misc.ApiQuery
-	console.log("/api/tasks query:", data)
+	console.log(`[${req_id}] GET /api/tasks query:`, data)
 
 	const handler = handlers[data.comp]
 	if (handler === undefined) {
+		console.log(`[${req_id}] GET /api/tasks => 400 Invalid competition`)
 		res.status(400).send("Invalid competition")
 		return
 	}
 	
 	handler.get_tasks(data)
-		.then(events => res.json(events))
+		.then(events => {
+			console.log(`[${req_id}] GET /api/tasks => 200`)
+			res.json(events)
+		})
 		.catch(err => {
+			console.log(`[${req_id}] GET /api/tasks => 500 Internal error`)
 			res.status(500).send("Internal error probably caused by malformed request")
 			console.log(err)
 		})
 })
 
 app.get("/api/scores", (req, res) => {
+	const req_id = unique_req_id++;
+
 	const data: misc.ApiQuery = req.query as unknown as misc.ApiQuery
+	console.log(`[${req_id}] /api/scores query:`, data)
 
 	const handler = handlers[data.comp]
 	if (handler === undefined) {
+		console.log(`[${req_id}] GET /api/scores => 400 Invalid competition`)
 		res.status(400).send("Invalid competition")
 		return
 	}
 	
 	handler.get_scores(data)
-		.then(scores => res.json(scores))
+		.then(scores => {
+			console.log(`[${req_id}] GET /api/scores => 200`)
+			res.json(scores)
+		})
 		.catch(err => {
+			console.log(`[${req_id}] GET /api/scores => 500 Internal error`)
 			res.status(500).send("Internal error probably caused by malformed request")
 			console.log(err)
 		})
 })
 
 app.get("/api/list", (req, res) => {
-	const data: misc.ApiQuery = req.query as unknown as misc.ApiQuery
+	const req_id = unique_req_id++;
+	console.log(`[${req_id}] /api/list`)
 
 	const competition_list: misc.CompetitionInfo[] = []
 	for (const code in handlers) {
@@ -60,6 +77,7 @@ app.get("/api/list", (req, res) => {
 			competition_list.push({code, name: handlers[code].name, round: undefined})
 	}
 	
+	console.log(`[${req_id}] GET /api/list => 200`)
 	res.json(competition_list)
 })
 
