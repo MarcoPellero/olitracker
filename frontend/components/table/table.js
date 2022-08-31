70e4066eb053db78f8c3fc4c121f992d3602fc4f
marco <marco.pellero.it@gmail.com>
2022-08-31 22:48:28 +0200

    separated the code into smaller chunks; table is now balanced

--- frontend/components/table/table.js
+++ frontend/components/table/table.js
@@ -1,3 +1,61 @@
+function balance() {
+	/* edits the table to make all of the rows have the same number of columns
+	 *
+	 * FROM:
+	 * +---+---+---+
+	 * |   |   |   |
+	 * +---+---+---+---+
+	 * |   |   |   |   |
+	 * +---+---+---+---+
+	 *
+	 * TO:
+	 * +---+---+---+---+
+	 * |   |   |   |///|
+	 * +---+---+---+---+
+	 * |   |   |   |   |
+	 * +---+---+---+---+
+	*/
+
+	const table = document.querySelector("table")
+	const rows = Array.from(table.children)
+	let max_columns = 0
+
+	rows.forEach(row => {
+		if (row.children.length > max_columns)
+			max_columns = row.children.length
+	})
+
+	rows.forEach(row => {
+		for (let i = row.children.length; i < max_columns; i++)
+			row.append(document.createElement("td"))
+	})
+}
+
+function create_cell(task) {
+	const cell = document.createElement("td")
+
+	if (!task.link) {
+		cell.classList.add("unavailable")
+		cell.textContent = task.name
+		return cell
+	}
+
+	const link = document.createElement("a")
+	link.textContent = task.name
+	cell.append(link)
+
+	cell.classList.add("score-" +
+		(task.score === 100 ? "success"	:
+		task.score === 0 ? "fail"	:
+		task.score > 0 && task.score < 100 ? "mixed" :
+		"null")
+	)
+
+	if (task.score !== null) cell.style = `--percentage-done: ${task.score}%`
+
+	return cell
+}
+
 function populate(events) {
 	const table = document.querySelector("table")
 	table.innerHTML = "" // reset the table
@@ -9,33 +67,12 @@ function populate(events) {
 		header.textContent = ev.year
 		row.append(header)
 
-		ev.tasks.forEach(task => {
-			const cell = document.createElement("td")
-
-			if (!task.link) {
-				cell.classList.add("unavailable")
-				cell.textContent = task.name
-			} else {
-				const link = document.createElement("a")
-				link.href = task.link
-				link.textContent = task.name
-				cell.append(link)
-
-				if (task.score === null)
-					cell.classList.add("score-null")
-				else if (task.score === 0)
-					cell.classList.add("score-fail")
-				else if (task.score === 100)
-					cell.classList.add("score-success")
-				else if (task.score > 0 && task.score < 100) { // ignore "edge cases" (type errors?)
-					cell.classList.add("score-mixed")
-					cell.style = `--percentage-done: ${task.score}%`
-				}
-			}
-
-			row.append(cell)
-		})
+		ev.tasks.forEach(task => row.append(
+			create_cell(task)
+		))
 
 		table.append(row)
 	})
+
+	balance()
 }
