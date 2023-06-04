cece24803a195311d26d9443b601ef022b7354a8
Marco <marco.pellero.it@gmail.com>
2023-06-04 12:39:02 +0200

    initial normalized format & converter for oii

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
 mod oii;
 mod ois;
 
+struct Task {
+	name: String,
+	link: String
+}
+
+struct TaskGroup {
+	tasks: Vec<Task>,
+	id: String // for OII this'd be just the year, "2022", but for OIS, it could be "2022/1", year/round
+}
+
+struct Competition {
+	task_groups: Vec<TaskGroup>,
+	name: String
+}
+
+fn oii_normalize(contests: Vec<oii::Contest>) -> Competition {
+	let mut comp = Competition {
+		name: "oii".to_string(),
+		task_groups: Vec::new()
+	};
+
+	for contest in contests {
+		let mut group = TaskGroup {
+			id: contest.year.to_string(),
+			tasks: Vec::new()
+		};
+
+		for task in contest.tasks {
+			if task.link.is_none() {
+				println!("warning: oii{} task '{}' does not have a link and has therefore been skipped", contest.year, task.name);
+				continue;
+			}
+
+			group.tasks.push(Task {
+				name: task.name,
+				link: task.link.expect("The task to have a link")
+			});
+		}
+
+		if group.tasks.is_empty() {
+			println!("warning: oii{} has been completely skipped because none of its tasks have links", contest.year);
+			continue;
+		}
+
+		comp.task_groups.push(group);
+	}
+
+	return comp;
+}
+
 async fn print_oii_overview() {
 	let dump = match oii::get_contests().await {
 		Ok(v) => v,
@@ -33,5 +83,8 @@ async fn print_ois_overview() {
 
 #[tokio::main]
 async fn main() {
-	print_ois_overview().await;
+	let contests = match oii::get_contests().await {
+		Ok(v) => oii_normalize(v),
+		Err(e) => panic!("{}", e)
+	};
 }
