5bd5c2ca6ba85eba99519b46b376d80e96a7481a
Marco <marco.pellero.it@gmail.com>
2023-08-28 03:58:58 +0200

    dump data to file + allow null (Option<String>) links

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-use std::{cmp::min, cmp::max};
-use futures::{future::*, join};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use futures::future::*;
+use serde::Serialize;
 
 mod oii;
 mod ois;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Task {
 	name: String,
-	link: String
+	link: Option<String>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct TaskGroup {
 	tasks: Vec<Task>,
 	id: String // for OII this'd be just the year, "2022", but for OIS, it could be "2022/1", year/round
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Competition {
 	task_groups: Vec<TaskGroup>,
 	name: String
 }
 
-fn oii_normalize(contests: &Vec<oii::Contest>) -> Competition {
+async fn get_oii_normalized() -> Result<Competition, String> {
+	let contests = oii::get_contests().await?;
+
 	let mut comp = Competition {
 		name: "oii".to_string(),
 		task_groups: Vec::new()
@@ -35,34 +39,38 @@ fn oii_normalize(contests: &Vec<oii::Contest>) -> Competition {
 		};
 
 		for task in contest.tasks.iter() {
-			if task.link.is_none() {
-				println!("warning: oii{} task '{}' does not have a link and has therefore been skipped", contest.year, task.name);
-				continue;
-			}
-
 			group.tasks.push(Task {
 				name: task.name.clone(),
-				link: task.link.clone().expect("The task to have a link")
+				link: task.link.clone()
 			});
-		}
-
-		if group.tasks.is_empty() {
-			println!("warning: oii{} has been completely skipped because none of its tasks have links", contest.year);
-			continue;
 		}
 
 		comp.task_groups.push(group);
 	}
 
-	return comp;
+	return Ok(comp);
 }
 
-fn ois_normalize(editions: &Vec<&ois::Edition>) -> Vec<Competition> {
+async fn get_ois_normalized() -> Result<Vec<Competition>, String> {
+	let info = ois::get_info().await?;
+
+	let edition_responses = join_all(
+		info.editions
+		.iter()
+		.map(|ed| ois::get_edition(ed.num_id))
+	).await;
+
+	let raw_editions: Vec<&ois::Edition> = edition_responses
+		.iter()
+		.map(|ed| ed.as_ref().unwrap())
+		.collect();
+
 	let mut comps = Vec::new();
+
 	// i don't wanna hard-code the number of rounds :(
 	// it starts out with no rounds, and for each competition, i'll add them as i go
 	
-	for ed in editions {
+	for ed in raw_editions {
 		let year_str = ed.year_str.split('/').next().unwrap(); // 2022/23 -> 2022
 
 		for (i, round) in ed.rounds.iter().enumerate() {
@@ -79,7 +87,7 @@ fn ois_normalize(editions: &Vec<&ois::Edition>) -> Vec<Competition> {
 					.iter()
 					.map(|t| Task {
 						name: t.name.clone(),
-						link: format!("https://training.olinfo.it/#/task/ois_{}", t.name)
+						link: Some(format!("https://training.olinfo.it/#/task/ois_{}", t.name))
 					})
 					.collect()
 			};
@@ -88,27 +96,40 @@ fn ois_normalize(editions: &Vec<&ois::Edition>) -> Vec<Competition> {
 		}
 	}
 
-	return comps;
+	return Ok(comps);
+}
+
+fn dump_competitions(competitions: &Vec<Competition>, path: &str) -> Option<String> {
+	// if there was an error, it returns it. if not, it returns None
+
+	let file = match File::create(path) {
+		Ok(v) => v,
+		Err(e) => return Some(e.to_string())
+	};
+
+	let mut writer = BufWriter::new(file);
+	
+	let serialization_err = serde_json::to_writer(&mut writer, &competitions).err();
+	if serialization_err.is_some() {
+		return Some(serialization_err.unwrap().to_string());
+	}
+
+	let write_err = writer.flush();
+	if write_err.is_err() {
+		return Some(write_err.err().unwrap().to_string());
+	}
+
+	None
 }
 
 #[tokio::main]
 async fn main() {
-	let info = ois::get_info().await.unwrap();
-	println!("{} Editions", info.editions.len());
+	let mut competitions = Vec::new();
+	competitions.push(get_oii_normalized().await.unwrap());
+	competitions.extend(get_ois_normalized().await.unwrap());
 
-	let ed_resps = join_all(
-		info.editions
-		.iter()
-		.map(|ed| ois::get_edition(ed.num_id))
-	).await;
-	
-	let eds: Vec<&ois::Edition> = ed_resps
-		.iter()
-		.map(|ed| ed.as_ref().unwrap())
-		.collect();
-
-	let comps = ois_normalize(&eds);
-
-	let round = ois::get_round(12, "final").await.unwrap();
-	println!("Round: {:?}", round);
+	let dump_err = dump_competitions(&competitions, "data.json");
+	if dump_err.is_some() {
+		panic!("{}", dump_err.unwrap());
+	}
 }
